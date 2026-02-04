d7d87adb9a8b62744ac9bbc0eda20d5177fee4c6
Jérôme Gamez <jerome@gamez.name>
2026-02-04 20:17:18 +0100

    feat: add Vertex client (#264)
    * Add Vertex client
    
    * Add streaming support
    
    * Add Vertex model cases
    
    * Adapt implementation to latest changes
    
    * merge
    
    ---------
    
    Co-authored-by: dogisgreat <git@bigly.dog>

--- src/Messages/Model.php
+++ src/Messages/Model.php
@@ -8,9 +8,12 @@ namespace Anthropic\Messages;
  * The model that will complete your prompt.\n\nSee [models](https://docs.anthropic.com/en/docs/models-overview) for additional details and options.
  *
  * @see https://platform.claude.com/docs/en/build-with-claude/claude-on-amazon-bedrock AWS Bedrock model identifiers
+ * @see https://platform.claude.com/docs/en/build-with-claude/claude-on-vertex-ai List of supported Vertex AI models
  */
 enum Model: string
 {
+    // Anthropic model identifiers
+
     case CLAUDE_OPUS_4_5_20251101 = 'claude-opus-4-5-20251101';
 
     case CLAUDE_OPUS_4_5 = 'claude-opus-4-5';
@@ -82,4 +85,29 @@ enum Model: string
     case BEDROCK_CLAUDE_3_5_HAIKU_20241022 = 'anthropic.claude-3-5-haiku-20241022-v1:0';
 
     case BEDROCK_CLAUDE_3_HAIKU_20240307 = 'anthropic.claude-3-haiku-20240307-v1:0';
+    // Vertex AI model identifiers
+
+    case VERTEX_CLAUDE_OPUS_4_5_20251101 = 'claude-opus-4-5@20251101';
+
+    case VERTEX_CLAUDE_OPUS_4_1_20250805 = 'claude-opus-4-1@20250805';
+
+    case VERTEX_CLAUDE_OPUS_4_20250514 = 'claude-opus-4@20250514';
+
+    case VERTEX_CLAUDE_SONNET_4_5_20250929 = 'claude-sonnet-4-5@20250929';
+
+    case VERTEX_CLAUDE_SONNET_4_20250514 = 'claude-sonnet-4@20250514';
+
+    /**
+     * @deprecated Model is deprecated. Migrate to claude-sonnet-4@20250514.
+     */
+    case VERTEX_CLAUDE_3_7_SONNET_20250219 = 'claude-3-7-sonnet@20250219';
+
+    case VERTEX_CLAUDE_HAIKU_4_5_20251001 = 'claude-haiku-4-5@20251001';
+
+    /**
+     * @deprecated Model is deprecated. Migrate to claude-haiku-4-5@20251001.
+     */
+    case VERTEX_CLAUDE_3_5_HAIKU_20241022 = 'claude-3-5-haiku@20241022';
+
+    case VERTEX_CLAUDE_3_HAIKU_20240307 = 'claude-3-haiku@20240307';
 }
